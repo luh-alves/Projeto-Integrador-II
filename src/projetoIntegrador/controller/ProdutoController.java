6c49131eee55eea96b34b8de56b5044cd7921e21
Luciana Alves <luh.ana.af@gmail.com>
2019-11-03 20:28:46 -0300

    tentativa de resolução de conflitos

--- src/projetoIntegrador/controller/ProdutoController.java
+++ src/projetoIntegrador/controller/ProdutoController.java
@@ -5,17 +5,50 @@
  */
 package projetoIntegrador.controller;
 
+import java.util.ArrayList;
 import projetoIntegrador.model.dao.ProdutoDAO;
 import projetoIntegrador.model.entity.Produto;
 
-
 public class ProdutoController {
-
+    
     public static void salvarProduto(String nome, String quantidade, String valor) {
-        final Produto produto = new Produto();
-        produto.setNome(nome);
-        produto.setQuantidade(Integer.parseInt(quantidade));
-        produto.setValor(Double.parseDouble(valor));
+    final Produto produto = new Produto();
+    produto.setNome(nome);
+    produto.setQuantidade(Integer.parseInt(quantidade));
+    produto.setValor(Double.parseDouble(valor));       
+    }
+    //daqui p baixo codigo wellington 
+    public static boolean salvar(String NomeProduto, int Quantidade, double Valor) {
+
+        Produto p = new Produto(NomeProduto, Quantidade, Valor);
+        return ProdutoDAO.salvarP(p);
+    }
+
+    public static boolean excluir(int pID) {
+
+        return ProdutoDAO.excluirP(pID);
 
     }
+
+    public static boolean atualizar(int pID, String NomeProduto, int Quantidade, Double Valor) {
+
+        Produto p = new Produto(pID, NomeProduto, Quantidade, Valor);
+        return ProdutoDAO.atualizarP(p);
+
+    }
+
+    public static ArrayList<String[]> getProdutos() {
+
+        ArrayList<Produto> produtos = ProdutoDAO.getProdutos();
+        ArrayList<String[]> listaProdutos = new ArrayList<>();
+
+        for (int i = 0; i < produtos.size(); i++) {
+
+            Produto ProdutoString = produtos.get(i);
+
+            listaProdutos.add(new String[]{String.valueOf(produtos.get(i).getId()), produtos.get(i).getNome(), String.valueOf(ProdutoString.getQuantidade()), String.valueOf(ProdutoString.getValor())});
+        }
+        return listaProdutos;
+    }
+
 }
