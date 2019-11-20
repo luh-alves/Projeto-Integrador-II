999be389e813dd3c5b2be62e453f8bac524d07a3
wellington.eandrade <wellington.eandrade@CAS511761W1033.senacsp.edu.br>
2019-11-19 22:09:09 -0300

    Alteradas Sintético Controller e Sintético View

--- src/projetoIntegrador/controller/RelatorioSinteticoController.java
+++ src/projetoIntegrador/controller/RelatorioSinteticoController.java
@@ -5,12 +5,18 @@
  */
 package projetoIntegrador.controller;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 import projetoIntegrador.dao.VendaDAO;
 import projetoIntegrador.model.Venda;
+import projetoIntegrador.model.Cliente;
+import projetoIntegrador.utils.Database;
 
 /**
  *
@@ -18,15 +24,57 @@ import projetoIntegrador.model.Venda;
  */
 public class RelatorioSinteticoController {
 
-    public static ArrayList<Venda> buscaPorPeriodo(Date inicio, Date fim) {
-        ArrayList<Venda> vendas = new ArrayList<>();
-        for (Venda v : VendaDAO.consultarVendas()) {
+    public static ArrayList<Venda> consultarVendas(Date inicio, Date fim, Venda v) {
+        ResultSet rs = null;
+        Connection conexao = null;
+        PreparedStatement instrucaoSQL = null;
 
-            if (v.getDataVenda().after(inicio) && v.getDataVenda().before(fim)) {
-                vendas.add(v);
+        ArrayList<Venda> listaVendas = new ArrayList<Venda>();
+
+        try {
+
+            conexao = Database.abrirConexao();
+            instrucaoSQL = conexao.prepareStatement("SELECT * FROM venda BETWEEN ? AND ?");
+
+            instrucaoSQL.setDate(1, (java.sql.Date) v.getDataVenda());
+
+            rs = instrucaoSQL.executeQuery();
+
+            while (rs.next()) {
+                Venda c = new Venda();
+                c.setId(rs.getInt("id_venda"));
+                c.setDataVenda(rs.getDate("data_venda"));
+                c.setTotal(rs.getInt("total"));
+
+                Cliente obj = new Cliente();
+
+                obj.setId(rs.getInt("id_cli"));
+
+                c.setCliente(obj);
+
+                listaVendas.add(c);
+            }
+
+        } catch (SQLException | ClassNotFoundException ex) {
+            System.out.println(ex.getMessage());
+            listaVendas = null;
+        } finally {
+            //Libero os recursos da memória
+            try {
+                if (rs != null) {
+                    rs.close();
+                }
+                if (instrucaoSQL != null) {
+                    instrucaoSQL.close();
+                }
+
+                Database.fecharConexao();
+
+            } catch (SQLException ex) {
             }
         }
-        return vendas;
+
+        return listaVendas;
     }
 
     public static List<Venda> buscarVendas(Date date, Date date0) {
